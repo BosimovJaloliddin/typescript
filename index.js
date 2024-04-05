245ed980470b08677aacaf22d9bc3aa5e92923a9
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2024-04-05 10:52:49 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict";
+// let str:string="Hellow"
+// str.toUpperCase
+// let big:bigint = 77n
+// let title:null = null
+// title = undefined
+// let title:symbol = Symbol("hi")
+// title = Symbol()
+// let title:boolean = true
+// title = false
+// let obj: {age: number} = {age:12}
+// obj.age = 32
+// let arr: number[] = [1,2,3]
+// function f(name:string, age:number):string{
+//     return name
+// }
+// f("Hellow", 32).toUpperCase()
+// let num: string | number="1234"
+// console.log(Number(num).toFixed());
+// let language: "uzb" | "rus" | "eng" = "uzb"
+// const data: {id?: number, value: "uzb" | "rus" | "eng" }[] = [
+//     {value: "uzb"},
+//     {id:2, value: "eng"},
+//     {id:3, value: "rus"},
+// ]
+// data.map((lang) => {
+//     language = lang.value
+// })
+// const data: {id?: number, value?: string}[] = [
+//     {value: "uzb"},
+//     {id:2, value: "eng"},
+//     {id:3},
+// ]
