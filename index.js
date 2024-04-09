7b67f4b9a1728f671e8410c5add7f872ec03a322
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2024-04-09 12:02:20 +0500

    ts

--- index.js
+++ index.js
@@ -1,8 +1,100 @@
 "use strict";
 // let str:string="Hellow"
 // str.toUpperCase
-let arr = [
-    { name: "Ali", age: 12 },
-    { id: 2, name: "Ali", age: 12 },
-    { id: 3, name: "Ali", age: 12 },
-];
+// let big:bigint = 77n
+// let title:null = null
+// title = undefined
+// let title:symbol = Symbol("hi")
+// title = Symbol()
+// let title:boolean = true
+// title = false
+// let obj: {age: number} = {age:12}
+// obj.age = 32
+// let arr: number[] = [1,2,3]
+// function f(name:string, age:number):string{
+//     return name
+// }
+// f("Hellow", 32).toUpperCase()
+// let num: string | number="1234"
+// console.log(Number(num).toFixed());
+// let language: "uzb" | "rus" | "eng" = "uzb"
+// const data: {id?: number, value: "uzb" | "rus" | "eng" }[] = [
+//     {value: "uzb"},
+//     {id:2, value: "eng"},
+//     {id:3, value: "rus"},
+// ]
+// data.map((lang) => {
+//     language = lang.value
+// })
+// const data: {id?: number, value?: string}[] = [
+//     {value: "uzb"},
+//     {id:2, value: "eng"},
+//     {id:3},
+// ]
+// let nameing = 211;
+// function f(): string {
+//   return "hello";
+// }
+// type Fun = (title?: string, age?: number) => void;
+// let color: "oq" | "qora" | "qizil" = "oq";
+// type Color = "oq" | "qora" | "qizil";
+// interface Prop {
+//   id?: number;
+//   value: Color;
+// }
+// type DataProp = Prop[];
+// let data: DataProp = [
+//   { value: "oq" },
+//   { id: 2, value: "qora" },
+//   { id: 3, value: "qizil" },
+// ];
+// data.map((col) => (color = col.value));
+// let color: "oq" | "qora" | "qizil" = "oq";
+// type Ty = { name: string };
+// interface In {
+//   name: string;
+// }
+// interface In {
+//   age: number;
+// }
+// interface DataOne {
+//   id: number;
+//   name: string;
+// }
+// interface DataTwo extends DataOne {
+//   age: number;
+// }
+// interface DataThere extends DataOne {
+//   age: number;
+//   title: string;
+// }
+// const objOne: DataOne = {
+//   id: 1,
+//   name: "Ali",
+// };
+// const objTwo: DataTwo = {
+//   id: 1,
+//   name: "Ali",
+//   age: 32,
+// };
+// const objThere: DataThere = {
+//   id: 1,
+//   name: "Ali",
+//   age: 42,
+//   title: "Hellow",
+// };
+// interface Ar {
+//   id?: number;
+//   name: string;
+//   age: number;
+// }
+// type ArrayType = Ar[];
+// let arr: ArrayType = [
+//   { name: "Ali", age: 12 },
+//   { id: 2, name: "Ali", age: 12 },
+//   { id: 3, name: "Ali", age: 12 },
+// ];
+// let tupAr: [number, string, boolean] = [1, "Hi", true];
+// tupAr.reverse();
+// console.log(tupAr);
+let arr = [12];
